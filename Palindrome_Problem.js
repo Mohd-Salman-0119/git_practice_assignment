500ef03a667246c1ddff131f379e83cf82c78be2
Salman <mohdsalman309846@gmail.com>
2022-09-14 16:04:13 +0530

    Palindrome and Prime number program

---
+++ Palindrome_Problem.js
@@ -0,0 +1,9 @@
+let original_str = "MADAM";
+let reverse_str = 0;
+
+for(let i = original_str.length-1; i<=0; i++){
+  reverse_str = original_str[i]; 
+}
+revrse_str => origial_str ? console.log("Palindrome");console.log("Not Palindrome");
+
+  
---
+++ Prime_Problem.js
@@ -0,0 +1,11 @@
+let num = 23;
+
+for(let i = 1; i<=num; i++){
+		if(num%i===0){
+			count++;
+		}
+}
+if(count==1){
+	console.log('Prime);
+}else{
+	conole.log("Not Prime');
